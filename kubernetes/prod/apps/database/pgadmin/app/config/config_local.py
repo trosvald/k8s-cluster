18ad7b52b0a24b994423a073e24760215823dae4
trosvald <eko.pur25@gmail.com>
2024-02-13 18:00:43 +0700

    feat(k8s): add pgadmin

--- kubernetes/prod/apps/database/pgadmin/app/config/config_local.py
+++ kubernetes/prod/apps/database/pgadmin/app/config/config_local.py
@@ -2,7 +2,7 @@ AUTHENTICATION_SOURCES = ['oauth2', 'internal']
 OAUTH2_AUTO_CREATE_USER = True
 OAUTH2_CONFIG = [{
     'OAUTH2_NAME' : 'authentik',
-    'OAUTH2_DISPLAY_NAME' : 'Authentik',
+    'OAUTH2_DISPLAY_NAME' : 'SSO',
     'OAUTH2_CLIENT_ID' : '{{ .oidc_client_id }}',
     'OAUTH2_CLIENT_SECRET' : '{{ .oidc_client_secret }}',
     'OAUTH2_TOKEN_URL' : 'https://auth.monosense.io/application/o/token/',
@@ -11,6 +11,6 @@ OAUTH2_CONFIG = [{
     'OAUTH2_USERINFO_ENDPOINT' : 'https://auth.monosense.io/application/o/userinfo/',
     'OAUTH2_SERVER_METADATA_URL' : 'https://auth.monosense.io/application/o/pgadmin/.well-known/openid-configuration',
     'OAUTH2_SCOPE' : 'openid email profile',
-    'OAUTH2_ICON' : 'fa-solid fa-users',
-    'OAUTH2_BUTTON_COLOR' : '<button-color>'
+    'OAUTH2_ICON' : 'fa-user-plus',
+    'OAUTH2_BUTTON_COLOR' : '#fd4b2d'
 }]
